0180f6a2192399dd791430ad1302c91d7dcd1bce
Jeremie Veillet <jveillet@users.noreply.github.com>
2017-07-12 17:51:00 +0200

    [Core] Handle multiple pages for repositories (#33)

---
+++ app/datasources/github/pagination.rb
@@ -0,0 +1,23 @@
+module Datasources
+  module Github
+    ##
+    # Pagination of github results
+    #
+    class Pagination
+      attr_reader :records
+
+      def initialize(records)
+        @records = records
+      end
+
+      def next_pages(last_response)
+        results = @records
+        until last_response.rels[:next].nil?
+          last_response = last_response.rels[:next].get
+          results.concat last_response.data
+        end
+        results
+      end
+    end
+  end
+end
