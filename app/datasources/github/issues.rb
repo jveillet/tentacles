2ecc85eaa13de2c9896b1895af3e4f19a08d98cd
Jérémie Veillet <jveillet@users.noreply.github.com>
2017-07-27 23:49:24 +0200

    [Core] Handle deleted repositories in the pull requests list after a page refresh

--- app/datasources/github/issues.rb
+++ app/datasources/github/issues.rb
@@ -13,27 +13,45 @@ module Datasources
 
       def find_issues_by_repo(repository_name, access_token:,
                               per_page: PER_PAGE)
-        records = client(access_token).pull_requests(repository_name)
-        return [] if records.nil? || records.empty?
-        records
+        return [] unless repository_name
+        begin
+          records = client(access_token).pull_requests(repository_name)
+          return [] if records.nil? || records.empty?
+          records
+        rescue Octokit::NotFound => e
+          puts e.message
+          []
+        end
       end
 
       def find_labels_by_issue(repository_name, issue_number, access_token:)
-        labels = client(access_token).labels_for_issue(
-          repository_name,
-          issue_number
-        )
-        return [] if labels.nil? || labels.empty?
-        labels
+        return [] unless repository_name
+        begin
+          labels = client(access_token).labels_for_issue(
+            repository_name,
+            issue_number
+          )
+          return [] if labels.nil? || labels.empty?
+          labels
+        rescue Octokit::NotFound => e
+          puts e.message
+          []
+        end
       end
 
       def find_comments_by_issue(repository_name, issue_number, access_token:)
-        comments = client(access_token).pull_request_comments(
-          repository_name,
-          issue_number
-        )
-        return [] if comments.nil? || comments.empty?
-        comments
+        return [] unless repository_name
+        begin
+          comments = client(access_token).pull_request_comments(
+            repository_name,
+            issue_number
+          )
+          return [] if comments.nil? || comments.empty?
+          comments
+        rescue Octokit::NotFound => e
+          puts e.message
+          []
+        end
       end
     end
   end
